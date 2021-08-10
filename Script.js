bfb42c91d0f3346dcc8285435c539fd7bbb01842
Shella Batrisyia L <62591363+Shella2702@users.noreply.github.com>
2021-08-10 10:54:41 +0700

    Update Script.js

--- Script.js
+++ Script.js
@@ -70,30 +70,24 @@ searchInput.addEventListener('input', searchCountry)
 
 function requestUserRepos(username){
     
-  // Create new XMLHttpRequest object
+
   const xhr = new XMLHttpRequest();
-  
-  // GitHub endpoint, dynamically passing in specified username
+
   const url = `https://api.github.com/users/Shella2702`;
   
-  // Open a new connection, using a GET request via URL endpoint
-  // Providing 3 arguments (GET/POST, The URL, Async True/False)
+
   xhr.open('GET', url, true);
   
-  // When request is received
-  // Process it here
+
   xhr.onload = function() {
-  
-      // Parse API data into JSON
+ 
       const data = JSON.parse(this.response);
-      
-      // Log the response
+   
       console.log(data);
       
-      // Loop over each object in data array
+ 
       for (let i in data) {
-      
-          // Log the repo name
+     
           console.log('Repo:', data[i].name);
           
           // Log the repo description
@@ -109,10 +103,8 @@ function requestUserRepos(username){
 
   }
   
-  // Send the request to the server
   xhr.send();
   
 }
 
-// Call function passing in 'facebook' as GitHub username
 requestUserRepos('facebook');
